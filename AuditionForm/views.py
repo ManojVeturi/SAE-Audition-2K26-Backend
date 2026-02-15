f3a022bbdb354329f9bb20e538c82796d4fe1613
ManojVeturi <manojveturi2007@gmail.com>
2026-02-16 00:59:20 +0530

    fixed IsAuthenticated import

--- AuditionForm/views.py
+++ AuditionForm/views.py
@@ -21,7 +21,7 @@ from rest_framework.response import Response
 from rest_framework import status
 import random
 from django.core.mail import send_mail
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.decorators import api_view, permission_classes
