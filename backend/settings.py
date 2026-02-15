f72433c5ed996628eeb9357586359fdb571711a7
ManojVeturi <manojveturi2007@gmail.com>
2026-02-15 21:53:33 +0530

    changed cors

--- backend/settings.py
+++ backend/settings.py
@@ -62,7 +62,12 @@ MIDDLEWARE = [
 ]
 # CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-     "https://sae-audition-backend.up.railway.app", "http://localhost:5173", "https://sae-audition-2k26.vercel.app"
+    "http://localhost:5173",
+    "https://sae-audition-2k26.vercel.app",
+]
+
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://.*\.vercel\.app$",
 ]
 CSRF_TRUSTED_ORIGINS = [
     "https://sae-audition-backend.up.railway.app", "http://localhost:5173", "https://sae-audition-2k26.vercel.app"
