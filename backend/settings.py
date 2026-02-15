80b73dd8f5659dd33dbfb7ca4d1c19b90df2e995
ManojVeturi <manojveturi2007@gmail.com>
2026-02-16 01:17:16 +0530

    changes in cors

--- backend/settings.py
+++ backend/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
@@ -78,6 +78,7 @@ CORS_ALLOW_METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
 
 CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOW_HEADERS = ["*"]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
